87f0ab8b541b255238bc65d7766ad00301d65d9a
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-12-27 17:51:49 +0300

    header files also implemented as source files.

--- include/directory_ops.h
+++ include/directory_ops.h
@@ -2,6 +2,7 @@
 #define DIRECTORY_OPS_H
 
 void list_files(const char *path);
-
-
+void create_file_or_folder(const char *path);
+void delete_file_or_folder(const char *path);
+void write_to_file(const char *filepath, const char *content);
 #endif
